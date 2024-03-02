a07e04d627f6fd7518843709379ea08463f886e8
aleksanderwalczuk <aleksander.walczuk@therectangles.com>
2024-03-02 22:41:13 +0100

    reproduced link issue

---
+++ src/components/ButtonLink.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { RouteNamedMap } from "vue-router/auto/routes";
+import { RouterLinkProps } from "vue-router";
+
+const props = defineProps<
+  RouterLinkProps & { to: RouteNamedMap[keyof RouteNamedMap]["path"] }
+>();
+</script>
+<template>
+  <router-link
+    v-bind="props"
+    class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 pt-2 pb-[9px] text-xs leading-[15px] font-medium text-white shadow-sm active:bg-indigo-700 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    ><slot
+  /></router-link>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>Main Page</h1>
   <router-link to="/nested"> go to nested page</router-link>
+  <ButtonLink to="/nested"></ButtonLink>
 </template>
+<script setup lang="ts">
+import ButtonLink from "../components/ButtonLink.vue"
+</script>
 <route>
 {
   name: "Dashboard",
